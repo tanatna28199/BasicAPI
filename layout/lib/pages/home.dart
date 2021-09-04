007b778845fac0576c41ea79d0be28dfb0565a62
tanatna28199 <81291536+tanatna28199@users.noreply.github.com>
2021-09-04 21:00:41 +0700

    ๊Update ฟังก์ชั่น ดึงข้อมูล API from Github

--- layout/lib/pages/home.dart
+++ layout/lib/pages/home.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:layout/pages/detail.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   // const HomePage({ Key? key }) : super(key: key);
@@ -20,18 +21,18 @@ class _HomePageState extends State<HomePage> {
         body: Padding(
             padding: EdgeInsets.all(20),
             child: FutureBuilder(
-              builder: (context, snapshot) {
-                var data = json.decode(snapshot.data.toString());
+              builder: (context, AsyncSnapshot snapshot) {
+                //var data = json.decode(snapshot.data.toString());
                 return ListView.builder(
                   itemBuilder: (BuildContext context, int index) {
-                    return MyBox(data[index]['title'], data[index]['subtitle'],
-                        data[index]['image_url'], data[index]['detail']);
+                    return MyBox(snapshot.data[index]['title'], snapshot.data[index]['subtitle'],
+                        snapshot.data[index]['image_url'], snapshot.data[index]['detail']);
                   },
-                  itemCount: data.length,
+                  itemCount: snapshot.data.length,
                 );
               },
-              future:
-                  DefaultAssetBundle.of(context).loadString('assets/data.json'),
+              future: getData(),
+              //future:DefaultAssetBundle.of(context).loadString('assets/data.json'),
             )));
   }
 
@@ -81,5 +82,14 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  Future getData() async {
+    //https://raw.githubusercontent.com/tanatna28199/BasicAPI/main/data.json
+    var url = Uri.https(
+        'raw.githubusercontent.com', '/tanatna28199/BasicAPI/main/data.json');
+    var response = await http.get(url);
+    var result = json.decode(response.body);
+    return result;
   }
 }
